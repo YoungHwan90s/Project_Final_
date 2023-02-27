80465d0dff6f36c3742f2b64d7b7e78c1b31c3f1
wjdrl3122 <69457171+wjdrl3122@users.noreply.github.com>
2023-02-27 14:44:29 +0900

    Revert "Design"

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hellow YoungHwan Oh Oh!!')
-})
-
 // 라우터 선언
 // const auth = require('../middleware/auth');
 // const refreshToken = require('../middleware/refreshToken');
